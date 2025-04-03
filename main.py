49c3837d0fad91e2684bf59343d91e256a32c99a
henryburton1 <79949363+henryburton1@users.noreply.github.com>
2025-04-03 17:20:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -359,7 +359,7 @@ if __name__ == "__main__":
         publish_reports_to_metaculus=True,
         folder_to_save_reports_to=None,
         #Important to keep the following in mind:
-        skip_previously_forecasted_questions=True,
+        skip_previously_forecasted_questions=False,
         llms={
             "default": GeneralLlm(
                 model="openai/o3-mini",  # o3 mini does the forecasting
