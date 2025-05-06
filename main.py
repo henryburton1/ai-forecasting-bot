29d42d54fc19e052df41ad8e3472e23f48c844c7
henryburton1 <79949363+henryburton1@users.noreply.github.com>
2025-05-05 20:58:41 -0400

    Temporary predict on all questions

--- main.py
+++ main.py
@@ -361,7 +361,7 @@ if __name__ == "__main__":
         publish_reports_to_metaculus=True,
         folder_to_save_reports_to=None,
         #Important to keep the following in mind:
-        skip_previously_forecasted_questions=datetime.datetime.now().time() < datetime.time(23, 30),
+        skip_previously_forecasted_questions=False, #datetime.datetime.now().time() < datetime.time(23, 30),
         llms={
             "default": GeneralLlm(
                 # model="metaculus/openai/o3-mini",  # o3 mini does the forecasting
